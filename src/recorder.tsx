f5509ff55f9ab28c626cc7a86d16856d895b607d
Pritam <debnathpritam0802@gmail.com>
2025-05-26 09:49:51 +0530

    Improvements

--- src/recorder.tsx
+++ src/recorder.tsx
@@ -1,18 +1,17 @@
 import { Download, PauseCircle, Trash, ChevronDown, ChevronUp, Play, PlayCircle, StopCircle, } from "lucide-react";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { useReactMediaRecorder } from "react-media-recorder";
-import { useRef } from "react";
 
 
 const Recorder = () => {
+
+    const [isVideoPreviewOpen, setIsVideoPreviewOpen] = useState(false);
+    const nodeRef = useRef<HTMLDivElement>(null);
+    const isMobile = window?.matchMedia("(max-width: 767px)")?.matches;
     const { status, startRecording, pauseRecording, resumeRecording, stopRecording, clearBlobUrl, mediaBlobUrl, error } =
         useReactMediaRecorder({
             screen: true,
         });
-    const [isVideoPreviewOpen, setIsVideoPreviewOpen] = useState(false);
-    const nodeRef = useRef<HTMLDivElement>(null);
-
-
 
     if (error) {
         return (
@@ -21,6 +20,7 @@ const Recorder = () => {
             </div>
         )
     }
+
     // if the browser does not support screen recording
     if (!navigator?.mediaDevices || !navigator?.mediaDevices?.getDisplayMedia) {
         return (
@@ -32,16 +32,7 @@ const Recorder = () => {
     }
 
     // check if the device is mobile
-
-    const isMobile = () => {
-        const result = window?.matchMedia("(max-width: 767px)")?.matches;
-        if (result) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    if (isMobile()) {
+    if (isMobile) {
         return (
             <div className="flex flex-col items-center justify-center w-full h-full">
                 <h3 className="text-2xl font-bold text-center">This app does not support mobile devices</h3>
@@ -52,56 +43,48 @@ const Recorder = () => {
 
 
 
-
     return (
         <div
             ref={nodeRef}
             className={
-                "   z-50 flex p-2 w-[82px]--   flex-col rounded-lg border border-[#eaeaff] bg-white shadow-[0px_12px_16px_-4px_rgba(16,24,40,0.08),0px_4px_6px_-2px_rgba(16,24,40,0.03)] transition-all duration-500 ease-in-out overflow-hidden " +
+                "flex  flex-col rounded-lg border border-[#eaeaff] bg-white shadow-[0px_12px_16px_-4px_rgba(16,24,40,0.08),0px_4px_6px_-2px_rgba(16,24,40,0.03)] transition-all duration-500 ease-in-out overflow-hidden " +
                 (isVideoPreviewOpen
                     ? "max-h-[385px]     "
                     : (mediaBlobUrl)
                         ? "  "
-                        : "max-h-[50px]-- ") +
+                        : "  ") +
                 (status === "recording" ? "border-green-200 " : "") +
                 (status === "paused" ? "border-yellow-200 " : "")
             }
 
         >
 
-            {
-                (
-                    <div
-                        className={
-                            'flex  w-full items-center justify-center  transition-all duration-500 ease-in-out ' +
-                            ((isVideoPreviewOpen && mediaBlobUrl) ? ' h-[200px]  pb-2 ' : ' h-[0px]  ')
-                        }>
-                        <video
-                            className={"h-full w-full rounded-lg " +
-                                (isVideoPreviewOpen ? 'block' : 'hidden')
-                            }
-                            src={mediaBlobUrl}
-                            controls
-                        // loop
-                        // autoPlay
-                        />
-                    </div>
-                )
-            }
+            {(
+                <div
+                    className={
+                        'flex  w-full items-center justify-center  transition-all duration-500 ease-in-out ' +
+                        ((isVideoPreviewOpen && mediaBlobUrl) ? ' h-[200px]  pb-2 ' : ' h-[0px]  ')
+                    }>
+                    <video
+                        className={"h-full w-full rounded-lg " +
+                            (isVideoPreviewOpen ? 'block' : 'hidden')
+                        }
+                        src={mediaBlobUrl}
+                        controls
+                    // loop
+                    // autoPlay
+                    />
+                </div>
+            )}
 
             <div className="flex w-full items-center justify-between gap-2 min-h-[32px]">
-
                 {
                     status == "recording" ? (
-                        <>
-                            <button title="Pause" onClick={() => {
-                                pauseRecording()
-                            }}>
-                                <PauseCircle className="animate-pulse h-6 w-6 text-gray-500 hover:text-black" />
-                            </button>
-
-                        </>
-
+                        <button title="Pause" onClick={() => {
+                            pauseRecording()
+                        }}>
+                            <PauseCircle className="animate-pulse h-6 w-6 text-gray-500 hover:text-black" />
+                        </button>
                     ) : status != "paused" ? (
                         <button title="Start" onClick={() => {
                             startRecording()
@@ -109,12 +92,13 @@ const Recorder = () => {
                         }}>
                             <PlayCircle className="h-6 w-6 text-gray-500 hover:text-black" />
                         </button>
-                    ) : <>
+                    ) : (
                         <button title="Stop" onClick={() => {
                             stopRecording()
                         }}>
                             <StopCircle className="h-6 w-6 text-gray-500 hover:text-black" />
-                        </button></>
+                        </button>
+                    )
                 }
 
                 {
